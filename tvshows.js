a98eb4dbdc8c2d126fa30a3f142a5a3b84dc6f2e
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2025-04-21 17:15:37 +0530

    Switched to original images (instead of medium quality), switched layout from flex to grid

--- tvshows.js
+++ tvshows.js
@@ -1,3 +1,5 @@
+// const tvShows = ["The Watcher", "Stranger Things", "Dark", "Game of Thrones", "The Queen's Gambit", "House of the Dragon", "Normal People", "Adolescence", "The Penguin", "The Last of Us", "Loki", "WandaVision", "What If...?", "Moon Knight", "Hawkeye", "The Night Agent", "Fool Me Once", "The Summer I Turned Pretty", "A Good Girl's Guide To Murder", "The Haunting of Hill House", "3 Body Problem"];
+
 // axios
 // 	.get("https://api.tvmaze.com/search/shows?q=stranger+things")
 // 	.then((res) => {
@@ -12,14 +14,21 @@ const container = document.querySelector(".container");
 
 form.addEventListener("submit", async function (event) {
 	event.preventDefault();
-	console.log("Submitted...");
 	const query = this.elements.query.value;
 	this.elements.query.value = "";
+	await addTVShowImage(query);
+});
+
+const addTVShowImage = async (query) => {
 	const resultArray = await getTVShows(query);
 	let topURL;
+	if (!resultArray) {
+		console.log("Error getting a response from API");
+		return;
+	}
 	if (resultArray[0]) {
 		if (resultArray[0].show.image) {
-			topURL = resultArray[0].show.image.medium;
+			topURL = resultArray[0].show.image.original;
 		} else {
 			console.log(`No image found for "${query}"`);
 			return;
@@ -27,10 +36,11 @@ form.addEventListener("submit", async function (event) {
 		const img = document.createElement("img");
 		img.setAttribute("src", topURL);
 		container.appendChild(img);
+		console.log(`Added ${resultArray[0].show.name}`);
 	} else {
 		console.log(`No results found for "${query}"`);
 	}
-});
+};
 
 const getTVShows = async (query) => {
 	try {
@@ -38,7 +48,7 @@ const getTVShows = async (query) => {
 		const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
 		return res.data;
 	} catch (err) {
-		return "Error!";
+		return false;
 	}
 };
 
@@ -51,6 +61,6 @@ const getTVShowsWithoutAxios = async (query) => {
 		console.log(data);
 		return data;
 	} catch (err) {
-		return "Error!";
+		return false;
 	}
 };
